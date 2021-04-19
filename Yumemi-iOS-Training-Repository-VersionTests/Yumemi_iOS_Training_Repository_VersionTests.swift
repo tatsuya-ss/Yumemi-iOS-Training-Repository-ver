be13411fed3dbc00aed9e23dda2a65c3a22c5acd
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-04-19 16:21:45 +0900

    プロジェクト追加

---
+++ Yumemi-iOS-Training-Repository-VersionTests/Yumemi_iOS_Training_Repository_VersionTests.swift
@@ -0,0 +1,33 @@
+//
+//  Yumemi_iOS_Training_Repository_VersionTests.swift
+//  Yumemi-iOS-Training-Repository-VersionTests
+//
+//  Created by 坂本龍哉 on 2021/04/19.
+//
+
+import XCTest
+@testable import Yumemi_iOS_Training_Repository_Version
+
+class Yumemi_iOS_Training_Repository_VersionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
